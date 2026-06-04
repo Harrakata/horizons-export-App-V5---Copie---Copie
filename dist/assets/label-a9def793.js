c10716598e03561870cb98b94fc5a55c35a440db
Abdoul Rachid KADER <ar.kader@groupecarrus.com>
2026-06-04 11:26:07 +0200

    ajout granularité sous onglet profil et profil utilisateur

--- dist/assets/label-a9def793.js
+++
@@ -1,1 +0,0 @@
-import{j as a,u as e}from"./vendor-radix-9eb67e41.js";import{R as s}from"./vendor-react-144ec7f2.js";import{D as r,Y as o}from"./index-bbf39688.js";const d=o("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),m=s.forwardRef((({className:s,...o},m)=>a.jsx(e,{ref:m,className:r(d(),s),...o})));m.displayName=e.displayName;export{m as L};
