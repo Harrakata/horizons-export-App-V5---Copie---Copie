1f07f447342599d30645b3efbf7833d0a25d2f21
Abdoul Rachid KADER <ar.kader@groupecarrus.com>
2026-06-09 01:19:47 +0200

    ajout modification format mobil V7

--- dist/assets/label-0a2e837d.js
+++
@@ -1,1 +0,0 @@
-import{j as a,u as e}from"./vendor-radix-8576e9e6.js";import{R as s}from"./vendor-react-144ec7f2.js";import{L as r,ac as o}from"./index-df060c5c.js";const d=o("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),m=s.forwardRef((({className:s,...o},m)=>a.jsx(e,{ref:m,className:r(d(),s),...o})));m.displayName=e.displayName;export{m as L};
